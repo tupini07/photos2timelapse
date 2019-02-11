460391cf9d54eef86398cbb74a05d170c82886f2
Andrea Tupini <tupini07@gmail.com>
2019-02-11 20:46:14 +0100

    if there is a new version, download it to current folder

--- src/photos2timelapse/updater.clj
+++ src/photos2timelapse/updater.clj
@@ -5,6 +5,11 @@
 
 (def local-tag "1.0.1")
 
+(defn copy-uri-to-file [uri file]
+  (with-open [in (clojure.java.io/input-stream uri)
+              out (clojure.java.io/output-stream file)]
+    (clojure.java.io/copy in out)))
+
 (defn check-for-updates []
   (println "Checking for updates ..")
   (let [remote-tag (:tag_name (json/read-str
@@ -14,8 +19,16 @@
     (if (not= local-tag remote-tag)
 
       (do
-        (println (pt/green "A new version of this software is available! You can download it from")
-                 (pt/yellow (str "https://github.com/tupini07/photos2timelapse/releases/tag/" remote-tag)))
+        (def new-version-url (str "https://github.com/tupini07/photos2timelapse/releases/tag/" remote-tag))
+        (def new-filename (str "photos2timelapse-" remote-tag "-standalone.jar"))
+
+        (println (pt/green "A new version of this software is available! It can be found at")
+                 (pt/yellow new-version-url))
+
+        (println "New version will be downloaded to the current folder, as" new-filename)
+
+        (copy-uri-to-file (str new-version-url "/" new-filename) new-filename)
+
         (print "Since there is an update, do you want to stop the program? [N/y] ")
 
         (flush)
