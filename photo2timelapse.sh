33dc508d471bcdefe43a6970b5dbe77b6577614e
Andrea Tupini <tupini07@gmail.com>
2019-02-12 01:16:21 +0100

    download .jar file if not present with script

--- photo2timelapse.sh
+++ photo2timelapse.sh
@@ -1,3 +1,12 @@
+mkdir -p ~/.photo2timelapse/
+if [ $(ls ~/.photo2timelapse | grep .jar | wc -w) == 0 ]; then
+    echo "Application .jar file not found, downloading.. "
+    wget -O ~/.photo2timelapse/photos2timelapse-1.0.2-standalone.jar https://github.com/tupini07/photos2timelapse/releases/download/1.0.2/photos2timelapse-1.0.2-standalone.jar 
+    chmod +x ~/.photo2timelapse/photos2timelapse-1.0.2-standalone.jar
+
+    echo "Downloaded !"
+fi
+
 jars=($(ls ~/.photo2timelapse | grep .jar))
 latest=${jars[-1]}
 
@@ -5,7 +14,6 @@ java -jar ~/.photo2timelapse/$latest
 
 jar_in_current_folder=$(ls | grep photos2timelapse | grep .jar)
 if [ $(echo $jar_in_current_folder | wc -w) > 0 ]; then
-    mkdir -p ~/.photo2timelapse/
     mv $jar_in_current_folder ~/.photo2timelapse/
 fi
 
