7f91ed4163ea4a042ca770eec9a411cfe8f9e16b
Andrea Tupini <tupini07@gmail.com>
2019-07-29 21:57:02 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject photos2timelapse "1.0.3"
+(defproject photos2timelapse "1.0.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
